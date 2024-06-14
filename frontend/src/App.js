a6c68485aa9d3e3e26c44aae1009b41f7a018f6d
Ranger-HK <maxravindu99@gmail.com>
2024-06-15 01:25:24 +0530

    Update Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import Nav from './components/nav/Nav';
-import { RouteHandler } from './components/router/RouteHandler';
+import {RouteHandler} from './components/router/RouteHandler';
 import './App.css';
+import {Footer} from "./components/footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-     <Nav/>
-     <RouteHandler/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Nav/>
+            <RouteHandler/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
