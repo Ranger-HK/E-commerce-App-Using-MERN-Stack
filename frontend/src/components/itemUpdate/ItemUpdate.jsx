2caff270ed950f61ea9b927ff2e22d31b98500a4
Ranger-HK <maxravindu99@gmail.com>
2024-06-15 01:53:44 +0530

    Update Project

--- frontend/src/components/itemUpdate/ItemUpdate.jsx
+++ frontend/src/components/itemUpdate/ItemUpdate.jsx
@@ -1,96 +1,95 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { useParams} from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import {useNavigate, useParams} from 'react-router-dom';
 import axios from 'axios';
 import './itemUpdate.css'
 import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
 
 export default function FormPropsTextFields() {
 
-      const {id} = useParams()
-      const [itemCode, setItemCode] = useState()
-      const [itemName, setItemName] = useState()
-      const [itemPrice, setItemPrice] = useState()
-      const [qtyOnHand, setQtyOnHand] = useState()
-      const navigate = useNavigate();
-    
-      useEffect(() => {
-          axios.get("http://localhost:3500/api/v1/getItem/"+id)
-            .then(res => {console.log(res)
+    const {id} = useParams()
+    const [itemCode, setItemCode] = useState()
+    const [itemName, setItemName] = useState()
+    const [itemPrice, setItemPrice] = useState()
+    const [qtyOnHand, setQtyOnHand] = useState()
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get("http://localhost:3500/api/v1/getItem/" + id)
+            .then(res => {
+                console.log(res)
                 setItemCode(res.data.itemCode)
                 setItemName(res.data.itemName)
                 setItemPrice(res.data.itemPrice)
                 setQtyOnHand(res.data.qtyOnHand)
             })
             .catch(err => console.log(err))
-        }, [])
+    }, [])
 
 
-        const Update = (e) => {
-            e.preventDefault();
-            axios.put("http://localhost:3500/api/v1/updateItem/"+id, { itemCode, itemName, itemPrice, qtyOnHand })
+    const Update = (e) => {
+        e.preventDefault();
+        axios.put("http://localhost:3500/api/v1/updateItem/" + id, {itemCode, itemName, itemPrice, qtyOnHand})
             .then(res => {
-              // location.reload()
-              alert("Updated");
-              navigate('/admindash')
-      
+                // location.reload()
+                alert("Updated");
+                navigate('/admindash')
+
             })
             .catch(err => {
-              console.log(err)
-              alert("Failed");
+                console.log(err)
+                alert("Failed");
             })
-        }
+    }
 
-  return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-      onSubmit={Update}
-    >
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          //label="Item-code"
-          placeholder="item code"
-          value={itemCode}
-          onChange={(e) => setItemCode(e.target.value)}
-        />
-        <TextField
-         required
-         id="outlined-required"
-         //label="Item-name"
-         placeholder="item name"
-         value={itemName}
-         onChange={(e) => setItemName(e.target.value)}
-        />
-        <TextField
-         required
-         id="outlined-required"
-         //label="Item-price"
-         placeholder="item price"
-         value={itemPrice}
-         onChange={(e) => setItemPrice(e.target.value)}
-        />
-        <TextField
-         required
-         id="outlined-required"
-         //label="Qty-on-hand"
-         placeholder="qty on hand"
-         value={qtyOnHand}
-         onChange={(e) => setQtyOnHand(e.target.value)}
-        />
-      </div>
-      
-      <Button type="submit" variant="contained">Update</Button>
-    </Box>
-  );
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': {m: 10, width: '20ch'},
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={Update}
+        >
+            <div>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Item Code"
+                    defaultValue="Hello World"
+                    value={itemCode}
+                    onChange={(e) => setItemCode(e.target.value)}
+                />
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Item Name"
+                    defaultValue="Hello World"
+                    value={itemName}
+                    onChange={(e) => setItemName(e.target.value)}
+                />
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Item Price"
+                    defaultValue="Hello World"
+                    value={itemPrice}
+                    onChange={(e) => setItemPrice(e.target.value)}
+                />
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="QTY On Hand"
+                    defaultValue="Hello World"
+                    value={qtyOnHand}
+                    onChange={(e) => setQtyOnHand(e.target.value)}
+                />
+            </div>
+
+            <Button type="submit" color="success" variant="contained">Update</Button>
+        </Box>
+    );
 }
